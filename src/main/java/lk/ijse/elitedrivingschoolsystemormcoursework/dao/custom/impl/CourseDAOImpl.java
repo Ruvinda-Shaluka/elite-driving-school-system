523c0df07d2aa16143814b1355f8a55e93dc8101
Ruvinda24 <shalukaofficial24@gmail.com>
2025-09-04 10:28:09 +0530

    2nd commit on 4th September with CourseDAOImpl class completed

--- src/main/java/lk/ijse/elitedrivingschoolsystemormcoursework/dao/custom/impl/CourseDAOImpl.java
+++ src/main/java/lk/ijse/elitedrivingschoolsystemormcoursework/dao/custom/impl/CourseDAOImpl.java
@@ -1,45 +1,121 @@
 package lk.ijse.elitedrivingschoolsystemormcoursework.dao.custom.impl;
 
+import lk.ijse.elitedrivingschoolsystemormcoursework.config.FactoryConfiguration;
 import lk.ijse.elitedrivingschoolsystemormcoursework.dao.custom.CourseDAO;
-import lk.ijse.elitedrivingschoolsystemormcoursework.dao.custom.StudentsDAO;
 import lk.ijse.elitedrivingschoolsystemormcoursework.entity.Course;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public class CourseDAOImpl implements CourseDAO {
+    private final FactoryConfiguration factoryConfiguration = FactoryConfiguration.getInstance();
+
     @Override
     public List<Course> getAll() throws Exception {
-        return List.of();
+        Session session = factoryConfiguration.getSession();
+        try {
+            Query<Course> query = session.createQuery("from Course",Course.class);
+            List<Course> courseList = query.list();
+            return courseList;
+        }finally {
+            session.close();
+        }
     }
 
     @Override
     public String getLastId() throws Exception {
-        return "";
+        Session session = factoryConfiguration.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT c.course_id FROM Course c ORDER BY c.course_id DESC", String.class)
+                    .setMaxResults(1);
+            List<String> courseIdList = query.list();
+            if (courseIdList.isEmpty()) {
+                return null;
+            }
+            return courseIdList.get(0);
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public boolean save(Course course) throws Exception {
-        return false;
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.persist(course);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
     }
 
     @Override
     public boolean update(Course course) throws Exception {
-        return false;
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.merge(course);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            transaction.rollback();
+            return false;
+        }finally {
+            session.close();
+        }
     }
 
     @Override
     public boolean delete(String id) throws Exception {
-        return false;
+        Session session = factoryConfiguration.getSession();
+        Transaction transaction = session.beginTransaction();
+        try {
+            Course course = (Course) session.get(Course.class, id);
+            if (course != null) {
+                session.remove(course);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<String> getAllIds() throws Exception {
-        return List.of();
+        Session session = factoryConfiguration.getSession();
+        try {
+            Query<String> query = session.createQuery("SELECT c.course_id FROM Course c", String.class);
+            return query.list();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public Optional<Course> findById(String id) throws Exception {
-        return Optional.empty();
+        Session session = factoryConfiguration.getSession();
+        try {
+            Course course = session.get(Course.class, id);
+            return Optional.ofNullable(course);
+        } finally {
+            session.close();
+        }
     }
+
 }
