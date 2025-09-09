fe08d4a06a866580253a3a78cdf0b7a0cffbc906
Ruvinda24 <shalukaofficial24@gmail.com>
2025-09-09 11:03:27 +0530

    3rd commit on 9th September with LessonsBOImpl completed and some changes made in QueryDAO & QueryDAOImpl classes

--- src/main/java/lk/ijse/elitedrivingschoolsystemormcoursework/dao/custom/QueryDAO.java
+++ src/main/java/lk/ijse/elitedrivingschoolsystemormcoursework/dao/custom/QueryDAO.java
@@ -3,4 +3,5 @@ package lk.ijse.elitedrivingschoolsystemormcoursework.dao.custom;
 import lk.ijse.elitedrivingschoolsystemormcoursework.dao.SuperDAO;
 
 public interface QueryDAO extends SuperDAO {
+    public int getStudentCountForLesson(String lessonId);
 }
